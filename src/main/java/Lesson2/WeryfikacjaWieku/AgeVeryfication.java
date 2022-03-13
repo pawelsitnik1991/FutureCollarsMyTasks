7b27b43c082e63d5fda69d32460f332613ca7fbd
Paweł Sitnik <spkraski@gmail.com>
2022-03-13 14:07:31 +0100

    AgeVeryficationTest

--- src/main/java/Lesson2/WeryfikacjaWieku/AgeVeryfication.java
+++ src/main/java/Lesson2/WeryfikacjaWieku/AgeVeryfication.java
@@ -1,9 +1,7 @@
 package Lesson2.WeryfikacjaWieku;
 
-import java.util.Scanner;
-
 public class AgeVeryfication {
-    boolean verficationAge(int age){
+    boolean verficationAge(int age) {
         return age >= 18;
     }
 }
--- src/test/java/Lesson2/WeryfikacjaWieku/AgeVeryficationTest.java
+++ src/test/java/Lesson2/WeryfikacjaWieku/AgeVeryficationTest.java
@@ -6,22 +6,26 @@ import org.junit.jupiter.api.Assertions;
 public class AgeVeryficationTest {
     private AgeVeryfication ageVeryfication;
 
-    public AgeVeryficationTest(){
+    public AgeVeryficationTest() {
         ageVeryfication = new AgeVeryfication();
     }
 
     @Test
-    public void checkOutYouAreHaveMoreOrEqual18(){
-        int moreThen18age = 19;
-        int equal18age = 18;
-        int lessThen18age = 17;
+    public void shoudldRetrunTrueWhenAgeIsMoreThen18() {
+        int age = 19;
+        Assertions.assertTrue(ageVeryfication.verficationAge(age));
+    }
 
-        Assertions.assertTrue(ageVeryfication.verficationAge(moreThen18age));
-        Assertions.assertTrue(ageVeryfication.verficationAge(equal18age));
-        Assertions.assertFalse(ageVeryfication.verficationAge(lessThen18age));
+    @Test
+    public void shoudldRetrunTrueWhenAgeIsEqualTo18() {
+        int age = 18;
+        Assertions.assertTrue(ageVeryfication.verficationAge(age));
+    }
 
-
-
+    @Test
+    public void shoudldRetrunTrueWhenAgeIsLessThen18() {
+        int age = 17;
+        Assertions.assertFalse(ageVeryfication.verficationAge(age));
     }
 
 }
