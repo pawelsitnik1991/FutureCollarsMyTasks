087eb5d50721d417512b9a64cc802d0f95764cb2
Paweł Sitnik <spkraski@gmail.com>
2022-03-17 20:14:13 +0100

    Message.java

---
+++ src/main/java/Lesson4/MessageClass/ConsoleMessage.java
@@ -0,0 +1,8 @@
+package Lesson4.MessageClass;
+
+public class ConsoleMessage extends Message{
+    @Override
+    public String getMessage() {
+        return "This is class ConsoleMessage";
+    }
+}
