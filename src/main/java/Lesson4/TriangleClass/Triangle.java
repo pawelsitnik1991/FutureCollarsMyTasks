3abdbb3a2d6ac685c6f9bfb8f959fc07d90c7567
Paweł Sitnik <spkraski@gmail.com>
2022-03-19 15:18:31 +0100

    Triangle.java

---
+++ src/main/java/Lesson4/TriangleClass/Triangle.java
@@ -0,0 +1,15 @@
+package Lesson4.TriangleClass;
+
+public class Triangle {
+
+    double width;
+    double height;
+
+    @Override
+    public String toString() {
+        return "Triangle{" +
+                "width=" + width +
+                ", height=" + height +
+                '}';
+    }
+}
