b18f589648a42c7b005c029b583594314a0e6714
Paweł Sitnik <spkraski@gmail.com>
2022-03-19 14:38:29 +0100

    Animal.java

---
+++ src/main/java/Lesson4/AnimalClass/Animal.java
@@ -0,0 +1,10 @@
+package Lesson4.AnimalClass;
+
+public class Animal {
+
+    private String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/Lesson4/AnimalClass/Dog.java
@@ -0,0 +1,8 @@
+package Lesson4.AnimalClass;
+
+public class Dog extends Animal {
+
+    public Dog(String name) {
+        super(name);
+    }
+}
