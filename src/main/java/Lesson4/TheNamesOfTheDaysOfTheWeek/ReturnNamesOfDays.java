88de09e8da09f5ad3893bc02b27148766b23b8b2
Paweł Sitnik <spkraski@gmail.com>
2022-03-19 15:10:39 +0100

    DaysOfTheWeek.java

---
+++ src/main/java/Lesson4/TheNamesOfTheDaysOfTheWeek/ReturnNamesOfDays.java
@@ -0,0 +1,8 @@
+package Lesson4.TheNamesOfTheDaysOfTheWeek;
+
+public class ReturnNamesOfDays {
+
+    public String daysOfWeek(DaysOfTheWeek daysOfTheWeek){
+        return daysOfTheWeek.name();
+    }
+}
