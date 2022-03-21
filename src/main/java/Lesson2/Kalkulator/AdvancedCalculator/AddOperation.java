2a88dd5c8dd791b2e5334b8072461dba52044bca
Paweł Sitnik <spkraski@gmail.com>
2022-03-21 17:41:00 +0100

    AddOperationTest.java

---
+++ src/main/java/Lesson2/Kalkulator/AdvancedCalculator/AddOperation.java
@@ -0,0 +1,9 @@
+package Lesson2.Kalkulator.AdvancedCalculator;
+
+public class AddOperation implements Calculator {
+
+    @Override
+    public double advancedCalculator(int a, int b) {
+        return a + b;
+    }
+}
