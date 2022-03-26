6e1bf3fe380c200a91a85fc90edeecac196a1896
Paweł Sitnik <spkraski@gmail.com>
2022-03-26 13:51:36 +0100

    ConvertedToCapitalLetters.java -- Tworzenie pliku z wielkimi literami. Poprawnienie formatowania

--- src/main/java/Lesson6/Data/ConvertedToCapitalLetters.java
+++ src/main/java/Lesson6/Data/ConvertedToCapitalLetters.java
@@ -5,12 +5,12 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-public class ConvertedToCapitalLetters extends Data{
+public class ConvertedToCapitalLetters extends Data {
     private String path = "/Users/past/Desktop/Projekcik/NowaEra/src/main/java/Lesson6/Data/toUpperCaseWordFromFile.txt";
 
     public void returnFileWithConvertedToCapitalLetters() throws IOException {
         StringBuilder stringBuilder = new StringBuilder(readFromFile());
-        String toUpperCaseWordFromFile =stringBuilder.toString().toUpperCase();
+        String toUpperCaseWordFromFile = stringBuilder.toString().toUpperCase();
         System.out.println(toUpperCaseWordFromFile);
         File file = new File(path);
         file.createNewFile();
